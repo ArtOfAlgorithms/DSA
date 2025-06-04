bf8477c29a0d72be1c7715129aa0fc98c665f409
Param Chheda <paramkchheda@gmail.com>
2025-06-04 23:51:51 +0530

    string basics

---
+++ STRINGS/strings.java
@@ -0,0 +1,8 @@
+public class strings{
+    public static void main(String args[]){
+        String str = "Param";
+        String str2 = new String("Param");
+
+        // Strings are IMMUTABLE
+    }
+}
