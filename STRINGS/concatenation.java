917c1fd5faa8fa9bf090bd5ac23096accd17cc1f
Param Chheda <paramkchheda@gmail.com>
2025-06-05 23:54:34 +0530

    implemented concatenation

---
+++ STRINGS/concatenation.java
@@ -0,0 +1,8 @@
+public class concatenation {
+    public static void main(String args[]){
+        String firstname = "Param";
+        String lastname = "Chheda";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+    }
+}
